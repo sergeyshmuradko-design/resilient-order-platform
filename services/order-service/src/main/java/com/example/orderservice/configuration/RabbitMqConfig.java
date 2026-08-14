54ec8e960502629074089eab8bdde47386aab2cb
sergeyshmuradko-design <sergey.shmuradko@gmail.com>
2026-08-14 16:53:15 +0000

    Externalize local secrets with Vault and broker app users

--- services/order-service/src/main/java/com/example/orderservice/configuration/RabbitMqConfig.java
+++ services/order-service/src/main/java/com/example/orderservice/configuration/RabbitMqConfig.java
@@ -1,10 +1,5 @@
 package com.example.orderservice.configuration;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -26,46 +21,6 @@ public class RabbitMqConfig {
     public static final String ORDER_DLX = "order.dlx";
     public static final String ORDER_CREATED_DLQ = "order.created.dlx";
     public static final String ORDER_CREATED_DLQ_ROUTING_KEY = "order.created.dlq";
-
-    @Bean
-    public DirectExchange orderExchange() {
-        return new DirectExchange(ORDER_EXCHANGE);
-    }
-
-    @Bean
-    public DirectExchange orderDeadLetterExchange() {
-        return new DirectExchange(ORDER_DLX);
-    }
-
-    @Bean
-    public Queue orderCreatedQueue() {
-        return QueueBuilder.durable(ORDER_CREATED_QUEUE)
-            .withArgument("x-dead-letter-exchange", ORDER_DLX)
-            .withArgument("x-dead-letter-routing-key", ORDER_CREATED_DLQ_ROUTING_KEY)
-            .build();
-    }
-
-    @Bean
-    public Queue orderCreatedDeadLetterQueue() {
-        return QueueBuilder.durable(ORDER_CREATED_DLQ)
-            .build();
-    }
-
-    @Bean
-    public Binding orderCreatedBinding() {
-        return BindingBuilder
-            .bind(orderCreatedQueue())
-            .to(orderExchange())
-            .with(ORDER_CREATED_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding orderCreatedDeadLetterBinding() {
-        return BindingBuilder
-            .bind(orderCreatedDeadLetterQueue())
-            .to(orderDeadLetterExchange())
-            .with(ORDER_CREATED_DLQ_ROUTING_KEY);
-    }
 
     @Bean
     public MessageConverter messageConverter() {
